8e5306135a6be603b665629fa1266665eb11b3d8
Sorin <sorin@q3.ro>
2019-05-06 13:28:17 +0300

    new pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.jpg'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -67,7 +67,7 @@ const Navbar = class extends React.Component {
                 Products
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                News & Events
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
