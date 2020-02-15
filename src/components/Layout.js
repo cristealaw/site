a92f5a8d4f64dc675454c55be7b6680107af47fd
cristealaw <50302310+cristealaw@users.noreply.github.com>
2020-02-15 12:19:11 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -42,7 +42,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content="/img/logo.jpg" />
+        <meta property="og:image" content="/img/logo1.jpg" />
       </Helmet>
       <Navbar />
       <div>{children}</div>
