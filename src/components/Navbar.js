d432922c52f067013f7398029eefccfd7eeb3463
cristealaw <50302310+cristealaw@users.noreply.github.com>
2020-02-17 11:49:47 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,7 +60,7 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
+              <Link className="navbar-item" to="/about-maritime-law-in-romania">
                 About us
               </Link>
               <Link className="navbar-item" to="/lawyers">
